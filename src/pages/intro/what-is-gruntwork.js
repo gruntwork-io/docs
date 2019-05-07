520c98fda409c91c661b9b9f3d4003ff465e28f8
Rob Morgan <robbym@gmail.com>
2019-05-07 23:19:17 +0200

    add intro stub pages

---
+++ src/pages/intro/what-is-gruntwork.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
+
+const WhatIsGruntwork = () => (
+  <Layout>
+    <SEO
+      title="What is Gruntwork?"
+      keywords={[`gruntwork`, `aws`, `gcp`, `terraform`, `docker`]}
+    />
+    <h1>What is Gruntwork?</h1>
+    <p>TODO - write page content.</p>
+  </Layout>
+)
+
+export default WhatIsGruntwork
