0273e96d8d971a6a309d6be9208b4dddcdeddc3c
Rob Morgan <robbym@gmail.com>
2019-05-08 13:59:15 +0200

    fixed svg asset paths

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
       resolve: "gatsby-plugin-react-svg",
       options: {
         rule: {
-          include: /assets/, // See below to configure properly
+          include: `${__dirname}/src/assets/`,
         },
       },
     },
