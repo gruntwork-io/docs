d2b176c1d942b0a1516f492a96afac41626a1b07
Rob Morgan <robbym@gmail.com>
2019-05-08 14:39:35 +0200

    use a simplified header for launch

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -18,37 +18,6 @@ const Header = ({ siteTitle }) => (
             height="36"
           />
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarToggler"
-          aria-controls="navbarToggler"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <BurgerIcon className="hidden-lg" />
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarToggler">
-          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li className="nav-item active">
-              <Link to="/" className="nav-link">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/introduction/" className="nav-link">
-                Introduction
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/guides/" className="nav-link">
-                Guides
-              </Link>
-            </li>
-          </ul>
-        </div>
       </nav>
     </header>
   </>
