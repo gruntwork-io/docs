b0d843f5a10ad8b3a9a6cbfb85da5557f2391218
Rob Morgan <robbym@gmail.com>
2019-05-08 13:59:48 +0200

    save responsive header wip

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,113 +3,53 @@ import PropTypes from "prop-types"
 import React from "react"
 
 import Logo from "./logo.png"
-
-import Icon from "../Icon"
-import SearchIcon from "../../assets/glyphs/search.svg"
+import BurgerIcon from "../../assets/glyphs/burger.svg"
 
 const Header = ({ siteTitle }) => (
   <>
     <header>
-      <div class="navbar fixed-top bg-white flex-md-nowrap navbar-expand-lg justify-content-between p-2 border-bottom">
-        <div class="d-lg-none">
-          <a
-            class="navbar-toggler mr-1"
-            href=""
-            data-id="headers-4-sidebar-toggle"
-          >
-            <img src="./assets/glyphs/burger.svg" />
-          </a>
+      <nav className="navbar fixed-top bg-white navbar-expand-lg border-bottom">
+        <Link to="/" className="ml-3 navbar-brand">
+          <img
+            src={Logo}
+            alt="Gruntwork Docs"
+            className="d-inline-block align-top"
+            width="278"
+            height="36"
+          />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarToggler"
+          aria-controls="navbarToggler"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <BurgerIcon className="hidden-lg" />
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarToggler">
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li className="nav-item active">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/introduction/" className="nav-link">
+                Introduction
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/guides/" className="nav-link">
+                Guides
+              </Link>
+            </li>
+          </ul>
         </div>
-        <div>
-          <Link to="/" className="navbar-brand">
-            <img src={Logo} alt="Gruntwork Docs" width="278" height="36" />
-          </Link>
-        </div>
-        <div class="collapse navbar-collapse flex-grow-0 weight-500">
-          <div class="navbar-nav">
-            <div class="nav-item d-none d-lg-block">
-              <a class="nav-link text-dark pr-0" href="">
-                <Icon icon={SearchIcon} width={24} height={24} fill={"#333"} />
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="d-lg-none ml-auto">
-          <div class="nav-item">
-            <a class="nav-link text-dark pr-0" href="">
-              <Icon icon={SearchIcon} width={24} height={24} fill={"#000"} />
-            </a>
-          </div>
-        </div>
-      </div>
-      <div
-        class="navbar-side"
-        id="headers-4-sidebar"
-        data-id="headers-4-sidebar-close"
-      >
-        <div class="navbar-side-content d-flex flex-column justify-content-between">
-          <div class="weight-700">
-            <a
-              class="navbar-side-close mb-4"
-              href=""
-              data-id="headers-4-sidebar-close"
-            >
-              <img src="./assets/glyphs/close.svg" width="32" />
-            </a>
-            <div class="navbar-nav">
-              <div class="nav-item d-none d-lg-block">
-                <a class="nav-link text-dark pr-0" href="">
-                  <Icon
-                    icon={SearchIcon}
-                    width={24}
-                    height={24}
-                    fill={"#000"}
-                  />
-                </a>
-              </div>
-            </div>
-          </div>
-          <div>
-            <p class="mb-3">
-              <a class="transparent-invert-link" href="">
-                <img src="./assets/glyphs/instagram-small.svg" width="16" />
-              </a>
-              <a class="transparent-invert-link ml-2" href="">
-                <img src="./assets/glyphs/facebook-small.svg" width="16" />
-              </a>
-              <a class="transparent-invert-link ml-2" href="">
-                <img src="./assets/glyphs/twitter-small.svg" width="16" />
-              </a>
-              <a class="transparent-invert-link ml-2" href="">
-                <img src="./assets/glyphs/pinterest-small.svg" width="16" />
-              </a>
-              <a class="transparent-invert-link ml-2" href="">
-                <img src="./assets/glyphs/thumbler-small.svg" width="16" />
-              </a>
-            </p>
-            <p class="small mb-0">
-              <span class="d-block d-xl-inline text-muted mb-1 mb-xl-0 mr-2">
-                &copy; 2019 Acme. All right reserved.
-              </span>
-              <a class="text-muted mr-2" href="">
-                <span>Privacy Policy</span>
-              </a>
-              <a class="text-muted" href="">
-                <span>Terms of Service</span>
-              </a>
-            </p>
-          </div>
-        </div>
-        <div class="navbar-side-content weight-700">
-          <a
-            class="navbar-side-close mb-4"
-            href=""
-            data-id="headers-4-sidebar-close"
-          >
-            <img src="./assets/glyphs/close.svg" width="32" />
-          </a>
-        </div>
-      </div>
+      </nav>
     </header>
   </>
 )
--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,38 +1,14 @@
-/* eslint-disable react/forbid-prop-types */
 import React from "react"
-import { any, string } from "prop-types"
-import { library } from "@fortawesome/fontawesome-svg-core"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import {
-  faGithub,
-  faInstagram,
-  faLinkedin,
-  faMediumM,
-  faTwitter,
-  faYoutube,
-} from "@fortawesome/free-brands-svg-icons"
-import { faBars } from "@fortawesome/free-solid-svg-icons"
 
-// By setting things up like this react-fontawesome knows of the icons
-// I am also only using these icons from the libraries so less overhead.
-library.add(
-  faGithub,
-  faInstagram,
-  faLinkedin,
-  faMediumM,
-  faTwitter,
-  faYoutube,
-  faBars
+const Icon = props => (
+  <svg
+    viewBox={props.icon.viewBox}
+    width={props.width}
+    height={props.height}
+    fill={props.fill}
+  >
+    <use xlinkHref={`#${props.icon.id}`} />
+  </svg>
 )
 
-const SiteIcon = ({ nav, icon, size }) => (
-  <FontAwesomeIcon icon={icon} size={size} />
-)
-
-SiteIcon.propTypes = {
-  nav: string,
-  icon: any,
-  size: string,
-}
-
-export default SiteIcon
+export default Icon
