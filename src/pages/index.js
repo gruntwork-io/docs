53d269a42699dc3fdee9d3d367116b5533a4aee2
Rob Morgan <robbym@gmail.com>
2019-05-08 12:21:10 +0200

    tweak homepage content

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,9 +13,11 @@ const IndexPage = () => (
 
     <h1>Gruntwork Docs</h1>
     <p>
-      Welcome to the Gruntwork documentation site. Here you will find guides and documentation that will get you started
-      with our products. If it is your first time hearing about Gruntwork,
-      check out <a href="/introduction/what-is-gruntwork">What is Gruntwork?</a> to learn more about what we do.
+      Welcome to the Gruntwork documentation site. Here you will find guides and
+      documentation that will get you started with our products. If it is your
+      first time hearing about Gruntwork, check out{" "}
+      <a href="/introduction/what-is-gruntwork/">What is Gruntwork?</a> to learn
+      more about what we do.
     </p>
 
     <div class="row py-3">
@@ -24,10 +26,14 @@ const IndexPage = () => (
           <div class="card-body">
             <h2>Deploying a Dockerized app on GKE</h2>
             <p class="mb-3">
-              Learn how to deploy a production-grade GKE cluster on GCP and take a Node.js app, Dockerize it, and run it
-              in production on the cluster.
+              Learn how to deploy a production-grade GKE cluster on GCP and take
+              a Node.js app, Dockerize it, and run it in production on the
+              cluster.
             </p>
-            <a class="weight-500 action-link" href="/guides/deploying-a-dockerized-app-on-gke">
+            <a
+              class="weight-500 action-link"
+              href="/guides/deploying-a-dockerized-app-on-gke/"
+            >
               <span>Read more</span>
             </a>
           </div>
@@ -38,8 +44,9 @@ const IndexPage = () => (
           <div class="card-body">
             <h2>Deploying a Production Grade EKS Cluster</h2>
             <p class="mb-3">
-              Learn how to use Gruntwork modules to deploy and manage a production-grade EKS cluster. Includes: network
-              topology, worker ASG pools, IAM roles and RBAC.
+              Learn how to use Gruntwork modules to deploy and manage a
+              production-grade EKS cluster. Includes: network topology, worker
+              ASG pools, IAM roles and RBAC.
             </p>
             <Link
               to="/guides/deploying-a-production-grade-eks-cluster/"
@@ -50,24 +57,13 @@ const IndexPage = () => (
           </div>
         </div>
       </div>
-      <div class="col-12 col-md-4">
-        <div class="card shadow-sm">
-          <div class="card-body">
-            <h2>Running Software on GKE</h2>
-            <p class="mb-3">
-              Harmonious colour themes have built up as the collection has
-              evolved.
-            </p>
-            <a class="weight-500 action-link" href="">
-              <span>Read more</span>
-            </a>
-          </div>
-        </div>
-      </div>
     </div>
 
     <p>
-      <em>Don't see what you are looking for? Check out <a href="/support">How do I get help?</a></em>
+      <em>
+        Don't see what you are looking for? Check out{" "}
+        <Link to="/support/">How do I get help?</Link>
+      </em>
     </p>
   </Layout>
 )
