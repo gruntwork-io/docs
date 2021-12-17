3a8e0b05ef75e0c07610b4e54f1b4c528f7f394d
Eugene Kolnick <34349331+eak12913@users.noreply.github.com>
2021-12-16 20:50:20 -0500

    [APT-1611] Prepare for prod deployment (#112)
    * [APT-1611] Prepare for prod deployment
    
    This PR puts in the last bit of info needed to allow for deployments to production
    
    * Updated the stage deployment script to unset Google Analytics variables so that there's no attempt to do Google Analytics tracking in the staging environment.

--- scripts/push-to-s3-stage.sh
+++ scripts/push-to-s3-stage.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+# Cicle CI sets Google Analytics env variables. We want to unset them for stage
+# as they are meant only for prod use. We don't track Google Analytics in stage.
+unset CONFIG_GOOGLE_ANALYTICS_TRACKING_ID
+unset CONFIG_GOOGLE_ANALYTICS_ANONYMIZE_IP
+
 yarn --ignore-optional && yarn build
 
 aws s3 cp --recursive build/ s3://docs.dogfood-stage.com
