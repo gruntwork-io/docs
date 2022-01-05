ceb83b6f49e5fb10dbcab1520337a5c65a858fe5
Eugene K <eak12913@gmail.com>
2022-01-05 11:24:26 -0500

    Cleaned up config files

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -195,24 +195,12 @@ const config = {
       },
       algolia: algoliaConfig
         ? {
-            // If Algolia did not provide you any appId, use 'BH4D9OD16A'
             appId: algoliaConfig.appId,
-
-            // Public API key: it is safe to commit it
+            // Public API key: it is safe to commit it but we still source from
+            // config.
             apiKey: algoliaConfig.apiKey,
 
             indexName: algoliaConfig.indexName,
-
-            // Optional: see doc section below
-            // contextualSearch: true,
-
-            // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-            // externalUrlRegex: "github\\.com|gruntwork.io\\.com",
-
-            // Optional: Algolia search parameters
-            searchParameters: {},
-
-            //... other Algolia params
           }
         : undefined,
       zoomSelector: ".markdown img",
