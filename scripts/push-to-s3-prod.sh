addc6344a9e938b579c6c478b17cc36281fb3aec
Oreoluwa Agunbiade <ore@gruntwork.io>
2022-01-20 13:21:07 -0700

    Remove unnecessary posthog config (#147)

--- scripts/push-to-s3-prod.sh
+++ scripts/push-to-s3-prod.sh
@@ -10,7 +10,6 @@ export ALGOLIA_INDEX_NAME="docs_site_prod"
 export ALGOLIA_API_KEY="49a495ba4c210780a28feed306d05522" # This is a search only key. It is safe to check in.
 
 export CONFIG_POSTHOG_API_KEY="phc_zJYg43ctbPl1BzsnODjop1op9KSsctiGm6DNNt3zHZT"
-export CONFIG_POSTHOG_APP_URL="$CONFIG_SITE_URL"
 
 yarn --ignore-optional && yarn build
 
