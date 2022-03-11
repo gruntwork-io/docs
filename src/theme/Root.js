b1d11b865e3a077515193be83ce01a8335799eb3
Oreoluwa Agunbiade <21035422+oredavids@users.noreply.github.com>
2022-03-11 11:34:49 -0700

    [APT-1762] Exclude public repos from subscriber notification warnings (#219)
    * Exclude public repos from subscriber notification warnings

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -4,7 +4,71 @@ import {
   idOfNoticeLink,
 } from "/src/components/Modal.tsx"
 
-const gruntworkRepos = "https://github.com/gruntwork-io"
+const gruntworkGithubOrg = "https://github.com/gruntwork-io/"
+
+const publicGruntworkRepoNames = [
+  "bash-commons",
+  "cloud-nuke",
+  "docs",
+  "fetch",
+  "fetch-test-public",
+  "git-xargs",
+  "go-commons",
+  "gruntwork-installer",
+  "gruntwork-io.github.io",
+  "health-checker",
+  "helm-kubernetes-services",
+  "helm-charts",
+  "infrastructure-as-code-testing-talk",
+  "infrastructure-as-code-training",
+  "intro-to-terraform",
+  "kafka-health-check",
+  "knowledge-base",
+  "kubergrunt",
+  "microframework",
+  "module-ci-update-terraform-variable-test",
+  "pre-commit",
+  "private-tls-cert",
+  "sample-app-docker",
+  "sfdc-related-files-lightning",
+  "terraform",
+  "terraform-aws-couchbase",
+  "terraform-aws-utilities",
+  "terraform-google-ci",
+  "terraform-google-gke",
+  "terraform-google-load-balancer",
+  "terraform-google-network",
+  "terraform-google-sql",
+  "terraform-google-static-assets",
+  "terraform-hiera-like-example",
+  "terraform-kubernetes-helm",
+  "terraform-kubernetes-namespace",
+  "terraform-module-in-root-for-terragrunt-test",
+  "terraform-training-solutions",
+  "terragrunt",
+  "terragrunt-infrastructure-live-example",
+  "terragrunt-infrastructure-modules-example",
+  "terratest",
+  "terratest-helm-testing-example",
+  "toc",
+  "website-comments",
+]
+
+/**
+ * Checks if a link is referencing a known public repo
+ *
+ * @param string repoLink
+ * @return {boolean}
+ */
+const isPublicGruntworkRepo = (repoLink) => {
+  // Match a link prefixed by the gruntworkGithubOrg and capture the next path reference
+  const pattern = new RegExp(`^${gruntworkGithubOrg}(.*?)(\/|$)`)
+  // e.g for a given link https://github.com/gruntwork-io/docs/intro -> `docs`
+  const repoName = repoLink.match(pattern)[1]
+
+  // returns boolean
+  return publicGruntworkRepoNames.includes(repoName)
+}
 
 export const DONT_SHOW_PRIVATE_GITHUB_WARNING_KEY = "dontWarnGitHubLinks"
 
@@ -27,9 +91,12 @@ function Root({ children }) {
         return
       }
 
-      if (event.target.href && event.target.href.includes(gruntworkRepos)) {
+      if (
+        event.target.href &&
+        event.target.href.startsWith(gruntworkGithubOrg) &&
+        !isPublicGruntworkRepo(event.target.href)
+      ) {
         event.preventDefault()
-        console.log
         setExternalLink(event.target.href)
         setDisplaySubscriberNotice(true)
       }
