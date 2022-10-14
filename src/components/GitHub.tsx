530b4a8c21c28972425996083a0e24ae4ac12e61
Eugene K <eak12913@gmail.com>
2022-10-14 10:23:53 -0400

    Added types and comments to explain some reasoning behind approach.

--- src/components/GitHub.tsx
+++ src/components/GitHub.tsx
@@ -1,12 +1,21 @@
 import React, { useEffect, useState } from "react"
 import Admonition from "@theme/Admonition"
 
+ type Discussion = {
+  title: string;
+  body:string;
+  bodyHTML: string;
+  number: number;
+  answer: { bodyHTML: string, body:string };
+};
+
+
 type GitHubProps = {
-  json: any
+  discussion: Discussion
 }
 
-export const PWithData: React.FunctionComponent<GitHubProps> = ({ json }) => {
-  return <div dangerouslySetInnerHTML={{ __html: json }} />
+export const PWithData: React.FunctionComponent<{rawHTML:string}> = ({ rawHTML }) => {
+  return <div dangerouslySetInnerHTML={{ __html: rawHTML }} />
 }
 
 
@@ -21,39 +30,48 @@ const visuallyHidden:React.CSSProperties = {
   border: "0"
 }
 
-export const GitHub: React.FunctionComponent<GitHubProps> = ({ json }) => {
+export const GitHub: React.FunctionComponent<GitHubProps> = ({ discussion }) => {
   const [body, setBody] = useState("")
   const [answerBody, setAnswerBody] = useState("")
 
   useEffect(() => {
-    const rawAnswer:string = json.bodyHTML;
+    const rawAnswer:string = discussion.bodyHTML;
     const answerNoZenDeskLink = rawAnswer.replace(/<hr>\n<ins[\s\S]*<\/ins>/gim, "");
     
     setBody(answerNoZenDeskLink)
-    setAnswerBody(json.answer?.bodyHTML)
+    setAnswerBody(discussion.answer?.bodyHTML)
   }, [])
   return (
     <>
       <div>
         <a
-          href={`https://github.com/gruntwork-io/knowledge-base/discussions/${json.number}`}
+          href={`https://github.com/gruntwork-io/knowledge-base/discussions/${discussion.number}`}
           className="link-button">
           View full discussion in GitHub
         </a>
       </div>
-      <PWithData json={body} />
+      <PWithData rawHTML={body} />
 
 
       <div style={{marginTop: "2.5rem"}}>
         <Admonition type="tip" title="Answer">
-          <PWithData json={answerBody} />
+          <PWithData rawHTML={answerBody} />
         </Admonition>
       </div>
 
 
-
-      <p style={visuallyHidden}>{json.body}</p>
-      <p style={visuallyHidden}>{json.answer?.body}</p>
+      {
+      /** 
+       * The following two paragraphs are visually hidden. They are here only because we use them as the raw text that will be
+       * indexed by Algolia's crawlers. Above, we're using GitHub's actual HTML and injecting that directly into the DOM
+       * so that we can get all of the proper formatting of code blocks, lists, etc from GitHub without having to
+       * re-figure out how to render. Unfortunately, we were not able to inject this HTML directly into the raw `mdx`
+       * files as that resulted in loads of subsequent compilation errors when we tried to `build` the docs site.
+       *
+       */
+      }
+      <p style={visuallyHidden}>{discussion.body}</p>
+      <p style={visuallyHidden}>{discussion.answer?.body}</p>
     </>
   )
 }
