e0c9400ff3657269fce49944019cd7e0bafe834d
Oreoluwa Agunbiade <agunbiade.oreoluwa@gmail.com>
2023-03-21 16:18:41 -0600

    Install optional dependencies in stage and prod

--- scripts/push-to-s3-stage.sh
+++ scripts/push-to-s3-stage.sh
@@ -9,6 +9,6 @@ export CONFIG_GOOGLE_ANALYTICS_TRACKING_ID="UA-154792164-2"
 export ALGOLIA_INDEX_NAME="docs_site_stage"
 export ALGOLIA_API_KEY="8487ee2b8a8d59dfd7597854d562a38b" # This is a search only key. It is safe to check in.
 
-yarn --ignore-optional && yarn build
+yarn && yarn build
 
 aws s3 sync build/ s3://docs.dogfood-stage.com --delete # Delete option ensures deleted files get removed from the bucket
