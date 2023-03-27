b69f4a1cf6f75bd4cfb8aaa0d5f138380ece0336
Oreoluwa Agunbiade <agunbiade.oreoluwa@gmail.com>
2023-03-27 11:12:38 -0600

    Update test reference

--- tests/guides/stay-up-to-date/index.md.spec.ts
+++ tests/guides/stay-up-to-date/index.md.spec.ts
@@ -6,7 +6,7 @@ const stayUpToDateIndexFile = fs.readFileSync(
 )
 
 const stayUpToDateIndexFileInDocsSources = fs.readFileSync(
-  "docs_sources/guides/stay-up-to-date/index.md",
+  "_docs-sources/guides/stay-up-to-date/index.md",
   "utf8"
 )
 
@@ -14,13 +14,14 @@ const releasesPlaceholder =
   "<!-- replaced-by-docs-sourcer-automatically-do-not-edit -->"
 
 describe("Guides: stay-up-to-date index file", () => {
-  it("should have releases placeholder in only docs_sources", () => {
+  // This a temporary workaround to prevent the inadvertent changes to the
+  // placeholder or the index page in docs from being overwritten with the
+  // releases placeholder & committed to the main branch. The local plugin
+  // of the docs-sourcer package automatically copies `_docs-sources/` into `docs`
+  // When this test fails, revert the change made to `docs/guides/stay-up-to-date/index.md`
+  // or `_docs-sources/guides/stay-up-to-date/index.md`
+  it("should have releases placeholder in only _docs-sources", () => {
     expect(stayUpToDateIndexFileInDocsSources).toContain(releasesPlaceholder)
-
-    // This a temporary workaround to prevent the index.page from being overwritten
-    // with the releases placeholder & committed to the main branch. The local plugin
-    // of the docs-sourcer package automatically copies `_docs_sources_/` into `docs`
-    // When this test fails, revert the change made to `docs/guides/stay-up-to-date/index.md`
     expect(stayUpToDateIndexFile).not.toContain(releasesPlaceholder)
   })
 })
