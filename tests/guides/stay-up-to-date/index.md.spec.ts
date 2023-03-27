d64cfc932a4641cbbd1396ac661814898650e5ce
Oreoluwa Agunbiade <agunbiade.oreoluwa@gmail.com>
2023-03-27 10:44:02 -0600

    Revert inadvertent changes to stay-up-to-date guide's index page

---
+++ tests/guides/stay-up-to-date/index.md.spec.ts
@@ -0,0 +1,19 @@
+const fs = require("fs")
+
+const stayUpToDateIndexFile = fs.readFileSync(
+  "docs/guides/stay-up-to-date/index.md",
+  "utf8"
+)
+
+const releasesPlaceholder =
+  "<!-- replaced-by-docs-sourcer-automatically-do-not-edit -->"
+
+describe("Guides: stay-up-to-date index file", () => {
+  it("should not contain releases placeholder", () => {
+    // This a temporary workaround to prevent the index.page from being overwritten
+    // with the releases placeholder & committed to the main branch. The local plugin
+    // of the docs-sourcer package automatically copies `_docs_sources_/` into `docs`
+    // When this test fails, revert the change made to `docs/guides/stay-up-to-date/index.md`
+    expect(stayUpToDateIndexFile).not.toContain(releasesPlaceholder)
+  })
+})
