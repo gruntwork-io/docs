727df7fd6e7b1493a31953df44a557074df6795a
Maxwell Moon <moon.maxwell@gmail.com>
2023-05-05 07:27:34 -0700

    add sidebars for placeholder product docs

---
+++ sidebars/landing-zone.js
@@ -0,0 +1,16 @@
+const sidebar = [
+    {
+        label: "Landing Zones",
+        type: "category",
+        collapsible: false,
+        items: [
+            {
+                label: "Getting Started",
+                type: "doc",
+                id: "landing-zone/index",
+            }
+        ]
+    }
+]
+
+module.exports = sidebar
---
+++ sidebars/pipelines.js
@@ -0,0 +1,16 @@
+const sidebar = [
+    {
+        label: "Gruntwork Pipelines",
+        type: "category",
+        collapsible: false,
+        items: [
+            {
+                label: "Getting Started",
+                type: "doc",
+                id: "pipelines/index",
+            }
+        ]
+    }
+]
+
+module.exports = sidebar
