112a0fc980c0d0de188ab02305a8861c292c4c1a
Maxwell Moon <moon.maxwell@gmail.com>
2023-06-01 15:28:51 -0700

    Implement basic search and styles

--- docs/iac/reference/index.md
+++ docs/iac/reference/index.md
@@ -1,14 +1,19 @@
+import SearchArea from "/src/components/SearchArea"
+
 # Library Reference
 
-The Library Reference serves as the definitive index for all actively maintained Modules and Services within the Gruntwork Infrastructure as Code Library. This comprehensive reference provides a dedicated page for each module and service providing descriptions, detailed information on input and output variables, and sample code to help you get started.
+The Library Reference provides a complete listing of the modules, services, and tools included in the Gruntwork IaC Library. Find one that suits your needs, or suggest a module if you need something we’re missing.
 
 If you're already familiar with the IaC Library and are ready to dive right in, you can find the full Service Catalog and Module catalog reference in the left sidebar.
 
 For an introduction to the Gruntwork IaC Library, check out the [Overview](/iac/overview) page. This page introduces the concept of Modules and Services, clarifies their respective purposes, and offers guidance on when and how to effectively utilize them. The overview is a great starting point for understanding what the library can offer and how to best navigate it.
 
+<SearchArea />
+
+
 <!-- ##DOCS-SOURCER-START
 {
   "sourcePlugin": "local-copier",
-  "hash": "de75e3371081959e3f3d5c1917ecae87"
+  "hash": "8c641be6d80f8c3bddf3df81ace55f62"
 }
 ##DOCS-SOURCER-END -->
---
+++ src/components/SearchArea.tsx
@@ -0,0 +1,97 @@
+import algoliasearch from "algoliasearch/lite"
+import React, { PropsWithChildren } from "react"
+import {
+  Configure,
+  InstantSearch,
+  RefinementList,
+  SearchBox,
+  UseHitsProps,
+  useHits
+} from "react-instantsearch-hooks-web"
+import { Card } from "./Card"
+import { CardGroup } from "./CardGroup"
+
+import styles from "./SearchArea.module.css"
+import Grid from "./Grid"
+
+interface SearchAreaProps {
+  name: string
+  requirement?: "required" | "optional"
+  type: string
+}
+
+function CustomHits(props: UseHitsProps) {
+  const { hits, results, sendEvent } = useHits(props)
+
+  /*
+  Don't display search results where the module has been deprecated. We prefix the friendly name for these
+  modules with [DEPRECATED], so we filter for all hits where there is not a match for the prefix.
+  */
+  const activeHits = hits.filter((hit: any) => !hit.moduleFriendlyName.startsWith('[DEPRECATED]'))
+
+  /*
+  Don't display modules where the description contains a note.
+  These modules all state they are not intended to be used directly.
+  */
+  const onlyUseableModules = activeHits.filter((hit: any) => hit.moduleDescription.toLowerCase().indexOf("note") === -1)
+
+  return (
+    <CardGroup cols={3}>
+      {onlyUseableModules.map((hit: any) => {
+        return (
+          <Card
+            title={hit.moduleFriendlyName}
+            key={hit.moduleFriendlyName}
+            href={hit.docsRelativeUrl}
+            tags={[hit.mainRepoTitle]}
+            className={styles.card_container_max_width}
+          >
+            <div
+              className={styles.card_result}
+              dangerouslySetInnerHTML={{ __html: (hit.moduleDescription as string).substring(3,  (hit.moduleDescription as string).length - 3) }}
+            ></div>
+          </Card>
+        )
+      })}
+    </CardGroup>
+  )
+}
+
+export const SearchArea: React.FunctionComponent<
+  PropsWithChildren<SearchAreaProps>
+> = ({ name, requirement, type, children }) => {
+  const algoliaAppId: string = "7AWZHGNJE2"
+  // This key is for search only. It is safe to check in.
+  const algoliaSearchKey: string = "a976ea48057ceaa662656ec8f4f591af"
+
+  const searchClient = algoliasearch(algoliaAppId, algoliaSearchKey)
+
+  return (
+    <div className={styles.container}>
+      <InstantSearch
+        searchClient={searchClient}
+        indexName="dev_docs_sourcer-modules"
+      >
+        <Configure facetFilters={["type:module"]} hitsPerPage={28} restrictSearchableAttributes={["mainRepoTitle", "moduleFriendlyName", "rawModuleName"]} />
+
+        <Grid cols={3}>
+          <div className={styles.SearchContainerItem}>
+            <p className={styles.SearchContainerItemHeader}>SEARCH</p>
+            <SearchBox placeholder="Try searching for VPC or EKS..." classNames={{input: styles.SearchInput, submit:styles.SearchSubmit, reset: styles.SearchReset}} />
+          </div>
+          <div className={styles.SearchContainerItem} id="sme-area">
+            <p className={styles.SearchContainerItemHeader}>TOPIC</p>
+            <RefinementList attribute="mainRepoTitle" searchable={false} className={styles.FacetListInput} />
+          </div>
+          <div className={styles.SearchContainerItem} id="type">
+            <p className={styles.SearchContainerItemHeader}>TYPE</p>
+            <RefinementList attribute="type" searchable={false} className={styles.FacetListInput} />
+          </div>
+        </Grid>
+        <CustomHits />
+      </InstantSearch>
+    </div>
+  )
+}
+
+export default SearchArea
