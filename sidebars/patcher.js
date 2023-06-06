94383434d9f6993116e79b5c8e2aebe434b2b1e7
techpink <7670638+techpink@users.noreply.github.com>
2023-06-06 13:41:39 +0200

    Added update strategies to sidebar

--- sidebars/patcher.js
+++ sidebars/patcher.js
@@ -1,38 +1,41 @@
 const sidebar = [
-    {
-      "Getting Started": [
-        {
-          "label": "Getting Started",
-          "type": "category",
-          "link": {
-            "type": "doc",
-            "id": "patcher/getting-started/index"
-          },
-          "items": []
-        }
-      ]
-    },
-    {
-      "Running Patcher": [
-        "patcher/running-patcher/report",
-        "patcher/running-patcher/update",
-        "patcher/running-patcher/upgrade"
-      ]
-    },
-    {
-      "Telemetry": [
-        {
-          "label": "Telemetry",
-          "type": "category",
-          "link": {
-            "type": "doc",
-            "id": "patcher/telemetry/index"
-          },
-          "items": []
-        }
-      ]
-    }
-  ]
+  {
+    "Getting Started": [
+      {
+        "label": "Getting Started",
+        "type": "category",
+        "link": {
+          "type": "doc",
+          "id": "patcher/getting-started/index"
+        },
+        "items": [
+          "patcher/getting-started/update-strategies"
+        ]
+      }
+    ]
+  },
+  {
+    "Running Patcher": [
+      "patcher/running-patcher/report",
+      "patcher/running-patcher/update",
+      "patcher/running-patcher/upgrade"
+    ]
+  },
+  {
+    "Telemetry": [
+      {
+        "label": "Telemetry",
+        "type": "category",
+        "link": {
+          "type": "doc",
+          "id": "patcher/telemetry/index"
+        },
+        "items": []
+      }
+    ]
+  }
+]
+
   
   module.exports = sidebar
 
