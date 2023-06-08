43bb5998fa88c6ea2932dee8353a036188c7d2a6
Oreoluwa Agunbiade <agunbiade.oreoluwa@gmail.com>
2023-06-08 15:05:52 -0600

    Remove working with code pages

--- sidebars/intro-guide.js
+++ sidebars/intro-guide.js
@@ -8,12 +8,6 @@ const sidebar = [
       "intro/overview/what-we-provide",
       "intro/overview/what-you-provide",
       "intro/overview/prerequisites",
-      // Temporarily hiding the unfinished sections from the sidebar We'll put
-      // them back shortly and don't want to delete the pages as we know we're
-      // going to have these sections within a few days.
-      // "intro/overview/gruntwork-production-framework",
-      // "intro/overview/use-cases",
-      // "intro/overview/gruntwork-vs-other",
       "intro/overview/getting-started",
     ],
   },
