5fb303613edf40c0b31a8dee49b01f32ba1992f2
Andrew Ellison <andrew@gruntwork.io>
2023-06-20 10:08:52 -0500

    remove mixpanel from tutorial (#902)

--- src/components/ModuleTutorialCodeBlock.tsx
+++ src/components/ModuleTutorialCodeBlock.tsx
@@ -12,10 +12,6 @@ export type ModuleTutorialCodeBlockProps = {
 export const ModuleTutorialCodeBlock: React.FC<
   ModuleTutorialCodeBlockProps
 > = ({ github_username, mixpanel_project_id }) => {
-  const mixpanelProjectId = "5736098d8918525aa0a75f1d6dda8321"
-  const buffer = new Buffer(mixpanelProjectId)
-  const base64data = buffer.toString("base64")
-
   const [githubUsername, setGithubUsername] = useState("unknown")
 
   const getGitHubUsernameFromQueryString = () => {
@@ -29,53 +25,42 @@ export const ModuleTutorialCodeBlock: React.FC<
   }
 
   const codeBlockSnippet = `import uuid
-import base64
 import json
 from urllib.request import urlopen, Request
 
 
 def lambda_handler(event, context):
-    url = "https://api.mixpanel.com/track"
+    url = "https://t.gruntwork.io/"
 
     github_username = "%github_username%"
 
-    # This code sets up our mixpanel project ID and sends an event into Mixpanel
-    # that includes your GitHub username to signify that you completed the tutorial
+    # This code sends an event to Gruntwork that includes your GitHub username to
+    # signify that you completed the tutorial.
     # We don't track anything else about you other than your GitHub username, and we
     # only use this data internally to understand who has completed our tutorial
-    mixpanelClientId = "%mixpanel_project_id%"
-    tok = base64.b64decode(mixpanelClientId).decode('utf-8')
 
-    payload = [
-    {
+    payload = {
+        "id": uuid.uuid4().hex,
         "event": "ModuleTutorialDeploymentComplete",
-        "properties": {
-        "token": tok,
-        "distinct_id": github_username,
-        "github_username": github_username,
-        "$insert_id": uuid.uuid4().hex
+        "eventProps": {
+            "distinct_id": github_username,
+            "github_username": github_username,
+            "$insert_id": uuid.uuid4().hex
         }
-    }]
+    }
 
     headers = {
       "accept": "text/plain",
       "content-type": "application/json"
     }
 
-    httprequest = Request(url, headers=headers,
+    httprequest = Request(url, headers=headers, method="POST",
     data=json.dumps(payload).encode('utf-8'))
 
     response_object = {}
 
     with urlopen(httprequest) as response:
-        text = ""
-        mixpanel_response_text = response.read().decode()
-        if mixpanel_response_text == "1":
-            text = "Success"
-        else:
-            text = "Unknown error"
-
-        response_object["operation_status"] = text
+        response_object["operation_status"] = "Success"
         response_object["statusCode"] = response.status
         response_object["body"] = f"Hello, {github_username}, from Gruntwork!"
 
@@ -86,7 +71,6 @@ def lambda_handler(event, context):
   const produceCodeSnippet = (githubUsername: string): string => {
     return codeBlockSnippet
       .replace("%github_username%", githubUsername)
-      .replace("%mixpanel_project_id%", base64data)
   }
 
   useEffect(() => {
