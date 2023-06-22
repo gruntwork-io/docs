709ce822b75d3157d39b4fc6390825f51bf94bec
Andrew Ellison <andrew@gruntwork.io>
2023-06-22 07:52:01 -0500

    switch to tag manager

--- scripts/push-to-s3-stage.sh
+++ scripts/push-to-s3-stage.sh
@@ -4,7 +4,7 @@ set -e
 
 export CONFIG_SITE_URL="https://docs.dogfood-stage.com"
 
-export CONFIG_GOOGLE_ANALYTICS_TRACKING_ID="UA-154792164-2"
+export CONFIG_GOOGLE_ANALYTICS_TRACKING_ID="GTM-WPZJH6M"
 
 export ALGOLIA_INDEX_NAME="docs_site_stage"
 export ALGOLIA_API_KEY="8487ee2b8a8d59dfd7597854d562a38b" # This is a search only key. It is safe to check in.
