a1be5d7237f5b03b85254c5156f2b19d2d7235e9
Maxwell Moon <moon.maxwell@gmail.com>
2023-10-02 08:42:09 -0700

    remove duplicate see the

--- src/components/OpenTofuNotice.tsx
+++ src/components/OpenTofuNotice.tsx
@@ -7,7 +7,7 @@ export const OpenTofuNotice = () => {
             <Admonition type="tip" title="OpenTofu Support">
                 <p>Gruntwork Library will support OpenTofu when OpenTofu becomes available.</p>
 
-                <p>To learn more about OpenTofu see the official see the <a href="https://opentofu.org/">OpenTofu website</a> and the current <a href="https://github.com/opentofu/opentofu/blob/main/WEEKLY_UPDATES.md">project status</a>.</p>
+                <p>To learn more about OpenTofu see the official <a href="https://opentofu.org/">OpenTofu website</a> and the current <a href="https://github.com/opentofu/opentofu/blob/main/WEEKLY_UPDATES.md">project status</a>.</p>
             </Admonition>
         </div>
     )
