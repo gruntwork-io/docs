724e2076d68e5cd177c841a1ebb7e8238d43ca43
Maxwell Moon <moon.maxwell@gmail.com>
2023-10-03 06:22:44 -0700

    Remove extra div

--- src/components/OpenTofuNotice.tsx
+++ src/components/OpenTofuNotice.tsx
@@ -3,11 +3,9 @@ import Admonition from "@theme/Admonition"
 
 export const OpenTofuNotice = () => {
     return (
-        <div>
-            <Admonition type="tip" title="OpenTofu Support">
-                <p>Gruntwork Library will support OpenTofu when it becomes available. To learn more, see the official <a href="https://opentofu.org/">OpenTofu website</a> and <a href="https://github.com/opentofu/opentofu/blob/main/WEEKLY_UPDATES.md">project status</a>.</p>
-            </Admonition>
-        </div>
+        <Admonition type="tip" title="OpenTofu Support">
+            <p>Gruntwork Library will support OpenTofu when it becomes available. To learn more, see the official <a href="https://opentofu.org/">OpenTofu website</a> and <a href="https://github.com/opentofu/opentofu/blob/main/WEEKLY_UPDATES.md">project status</a>.</p>
+        </Admonition>
     )
 }
 
