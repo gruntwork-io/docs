4dc27210fe77bfbd38efcea97fc2d2a15bdfbeca
Zach Goldberg <zach@zachgoldberg.com>
2024-10-31 14:56:24 -0700

    augment the url list, don't overwrite it, to preserve historic urls

--- utils/spider/spider_docs.sh
+++ utils/spider/spider_docs.sh
@@ -7,6 +7,12 @@ wget --spider --recursive --no-parent -o wget.log http://docs.gruntwork.io/
 awk '/^--/ {gsub(/\\/, "", $3); url=$3; sub(/\/$/, "", url); print url}' wget.log | sort -u  > full_urls_list.txt
 
 # Remove any static content
-awk '!/.(png|jpe?g|gif|svg|ico|css|js)$/' full_urls_list.txt > filtered_urls_list.txt
+awk '!/.(png|jpe?g|gif|svg|ico|css|js)$/' full_urls_list.txt >> filtered_urls_list.txt
+
+cp filtered_urls_list.txt combined_lists.txt
+
+cat combined_lists.txt | sort | uniq > filtered_urls_list.txt
+
+rm combined_lists.txt
 
 wc filtered_urls_list.txt
