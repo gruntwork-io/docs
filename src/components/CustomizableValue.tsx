7d1f5b73b6512091703ca7cf73e38f501f798f03
Lewis Christie <lewis.christie@gmail.com>
2024-11-01 13:40:48 -0600

    Move icon to css

--- src/components/CustomizableValue.tsx
+++ src/components/CustomizableValue.tsx
@@ -81,14 +81,8 @@ const CustomizableValue: React.FC<Props> = ({ id, placeholder }) => {
           style={{ border: '1px solid #ccc', padding: '4px', borderRadius: '4px' }}
         />
       ) : (
-        <span onClick={handleEditClick}>
+        <span className="customizable-value" onClick={handleEditClick}>
           {customValue || `<${placeholder}>`}
-          <span style={{ marginLeft: '4px' }}>
-            {/* Inline SVG for the pencil icon */}
-            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
-              <path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.7 1.8 18 9.5 20.5s18.7-5.4 20.5-9.5l20.9-37.4c4.2-7.9 11.8-15.8 22.2-22.2L291.7 184l62.1 62.1 33.9 33.9 11.3-11.3 113.4-113.4c14.1-14.1 23.3-31.8 22.2-51.4zM38.8 462l20.9-37.4c8.7-15.8 23.9-28 41.9-33.9L190.8 338l-11 11L58.6 322.9L38.8 462zm188-160l186.1-186.1L404.1 178l-22.6-22.6L178 355.4l-11 11L228.6 408l113.4-113.4z"/>
-            </svg>
-          </span>
         </span>
       )}
     </span>
