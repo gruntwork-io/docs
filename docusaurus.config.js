917729fc8a5b40eab88c4fde7e08cb8e7653c4c7
Oreoluwa Agunbiade <agunbiade.oreoluwa@gmail.com>
2025-10-06 14:31:04 -0600

    Cleanup posthog implementation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,11 +12,6 @@ const { redirects } = require("./src/redirects.js")
 
 const algoliaConfig = cfg.has("algolia") ? cfg.get("algolia") : undefined
 
-const posthogConfig = cfg.has("posthog") ? cfg.get("posthog") : undefined
-
-// 10-6-2025 - disabling posthog in docusaurus config as we deploy it to the page via Google Tag Manager instead
-const enablePosthog = false //posthogConfig && posthogConfig.apiKey
-
 const googleAnalyticsConfig = cfg.has("googleAnalytics")
   ? cfg.get("googleAnalytics")
   : undefined
@@ -34,10 +29,6 @@ const buildVersion = cfg.has("app.buildVersion")
   : "N/A"
 
 const plugins = ["plugin-image-zoom"]
-
-if (enablePosthog) {
-  plugins.push("posthog-docusaurus")
-}
 
 const redirectPlugin = [
   "@docusaurus/plugin-client-redirects",
@@ -449,12 +440,6 @@ async function createConfig() {
             }
           : undefined,
         zoomSelector: ".markdown :not(em) > img:not(.no-zoom)",
-        posthog: enablePosthog
-          ? {
-              apiKey: posthogConfig.apiKey,
-              appUrl: posthogConfig.appUrl,
-            }
-          : undefined,
         metadata: [
           // https://docusaurus.io/docs/2.x/seo#global-metadata
           // This would become <meta name="keywords" content="..."/> in the generated HTML
--- scripts/push-to-s3-prod.sh
+++ scripts/push-to-s3-prod.sh
@@ -9,8 +9,6 @@ export CONFIG_GOOGLE_ANALYTICS_TRACKING_ID="GTM-5TTJJGTL"
 export ALGOLIA_INDEX_NAME="docs_site_prod"
 export ALGOLIA_API_KEY="49a495ba4c210780a28feed306d05522" # This is a search only key. It is safe to check in.
 
-export CONFIG_POSTHOG_API_KEY="phc_ktdClyvZqCHAYrs5dIf1QmKKdswYqCknfrolP83cPjS"
-
 yarn && yarn build
 
 aws s3 sync build/ s3://docs.gruntwork.io --delete # Delete option ensures deleted files get removed from the bucket
